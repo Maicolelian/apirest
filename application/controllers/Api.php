9a02485af643ee5c2e714c66a51580a2b2592898
Maicol Muepaz <elianmuepaz29@gmail.com>
2025-03-18 19:49:48 -0500

    primer commit apirest

---
+++ application/controllers/Api.php
@@ -0,0 +1,34 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Api extends CI_Controller {
+
+	public function index()
+	{
+        $customers = $this->model->findAll();
+        return $this->respond($customers);
+	}
+
+    public function show($id = null) {
+        $$customer = $this->model->find($id);
+        if ($customer) {
+            return $this->respond($customer);
+        }
+    }
+
+    public function create() {
+        $model = new CustomerModel();
+        $data = $this->request->getJSON();
+        if ($model->insert($data)) {
+            $response = [
+                'status' => 201,
+                'error'  => null,
+                'messages' => [
+                    'success' => 'Cliente creado',
+                ],
+            ];
+            return $this->respondCreated($response);
+        }
+        return $this->fail($model->errors());
+    }
+}
